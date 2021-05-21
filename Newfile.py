5c4fa82a9c790ef377c941a4b6ddd0ca9e95a4c3
MuraliTony <84368585+MuraliTony@users.noreply.github.com>
2021-05-21 15:35:21 +0530

    Create Newfile.py

---
+++ Newfile.py
@@ -0,0 +1,21 @@
+name="murali"
+print("Hello {} !How are you?".format(name))
+age=18
+print("Age is %d" %(age))
+print("Hello %s !Your Age is %d"%(name,age))
+per=8.9
+print("Hello %s !Your Age is %d and your grade is %.2f"%(name,age,per))
+a=65
+print("hexa %x"%(a))
+print("hexa %X"%(a))
+print("char %c"%(a))
+print("oct %o"%(a))
+#output
+Hello murali !How are you?
+Age is 18
+Hello murali !Your Age is 18
+Hello murali !Your Age is 18 and your grade is 8.90
+hexa 41
+hexa 41
+char A
+oct 101
