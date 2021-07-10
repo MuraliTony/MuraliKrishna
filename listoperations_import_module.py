c7d5cbbff1fd42a3df9c60815926c5ab6f0b719e
MuraliTony <84368585+MuraliTony@users.noreply.github.com>
2021-07-10 15:21:45 +0530

    Create listoperations_import_module.py

---
+++ listoperations_import_module.py
@@ -0,0 +1,11 @@
+from listoperations_module import list_sum,list_sort,string_list_conversion
+list1=[345,236,543,257,547,578]
+print(list_sum(list1))
+print(list_sort(list1))
+print(string_list_conversion("Hello-World"))
+
+'''
+2506
+[345, 236, 543, 257, 547, 578]
+['H', 'e', 'l', 'l', 'o', '-', 'W', 'o', 'r', 'l', 'd']
+'''
